f9e569b22bfb4b719af6a39413e01f5169cf76e3
Karen Payne <paynekaren@comcast.net>
2023-01-14 05:59:47 -0800

    clean-up

--- DateOnlyApp1/Program.cs
+++ DateOnlyApp1/Program.cs
@@ -10,7 +10,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorPages();
 
         /*
@@ -28,23 +27,17 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.UseAuthorization();
-
         app.MapRazorPages();
-
         app.Run();
     }
 }
