712ef7726c6c364c77f4261e6bedc2959b30c220
Karen Payne <paynekaren@comcast.net>
2023-01-17 04:44:24 -0800

    added new project for serialize and deserialize System.Text.Json

---
+++ DateOnlyTimeOnlySysJsonApp/Models/VisitorLog.cs
@@ -0,0 +1,12 @@
+﻿namespace DateOnlyTimeOnlySysJsonApp.Models;
+
+public class VisitorLog
+{
+
+    public DateOnly VisitOn { get; set; }
+    public TimeOnly EnteredTime { get; set; }
+    public TimeOnly ExitedTime { get; set; }
+
+    public override string ToString()
+        => $"{VisitOn,-10}{EnteredTime,-10}{ExitedTime}";
+}
---
+++ DateOnlyTimeOnlySysJsonApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using DateOnlyTimeOnlySysJsonApp.Models;
+using Spectre.Console.Json;
+
+namespace DateOnlyTimeOnlySysJsonApp;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        VisitorLog log = new()
+        {
+            VisitOn = new DateOnly(2023,1,12), 
+            EnteredTime = new TimeOnly(13,15,15), 
+            ExitedTime = new TimeOnly(13,45,0)
+        };
+
+        
+        string jsonString = JsonSerializer.Serialize(log, new JsonSerializerOptions { WriteIndented = true });
+
+        var json = new JsonText(jsonString)
+            .BracketColor(Color.Green)
+            .ColonColor(Color.Blue)
+            .CommaColor(Color.Red)
+            .StringColor(Color.Green)
+            .NumberColor(Color.Blue)
+            .BooleanColor(Color.Red)
+            .NullColor(Color.Green);
+
+        AnsiConsole.Write(
+            new Panel(json)
+                .Header("VisitorLog serialized")
+                .Collapse()
+                .BorderColor(Color.White));
+
+        Console.WriteLine();
+
+        var deserializedLog = JsonSerializer.Deserialize<VisitorLog>(jsonString);
+        AnsiConsole.MarkupLine("[white]Deserialize[/]");
+        AnsiConsole.MarkupLine($"[yellow]Visited[/] {deserializedLog.VisitOn,-15}" + 
+                               $"[yellow]Entered[/] {deserializedLog.EnteredTime, -15}[yellow]Exit[/] {deserializedLog.ExitedTime, -15}");
+
+        Console.ReadLine();
+    }
+}
